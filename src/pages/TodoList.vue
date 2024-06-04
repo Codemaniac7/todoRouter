14d57bc6c298b06c3edbba12c65e1f6f7d05f53f
Jung Seung Min <jsm1592@gmail.com>
2024-06-05 07:18:10 +0900

    button html modify

--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -4,10 +4,10 @@
       <router-link class="btn btn-primary" to="/todos/add">
         할일 추가
       </router-link>
+      <button class="btn btn-primary ms-1" @click="fetchTodoList">
+        새로 고침
+      </button>
     </div>
-    <button class="btn btn-primary ms-1" @click="fetchTodoList">
-      새로 고침
-    </button>
   </div>
   <div class="row">
     <div class="col">
